acc49c63ffee20c48d269196c8385be3fd3738ff
Mr.Blood <kekcheburek335@gmail.com>
2024-09-14 08:51:47 +0300

    Update TGBot

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from settings import TG_TOKEN
+from telebot import *
+bot = telebot.TeleBot(TG_TOKEN)
+@bot.message_handler(commands = ['start'])
+def main(message):
+ bot.send_message(message.chat.id, '👋 Привет! Я твой помощник по маникюру! 💅\n С помощью меня ты можешь: \n\n * 💡 Посмотреть идеи для дизайна\n* 🗓 Записаться на процедуру к мастеру\n* 💅 Получить советы по уходу за ногтями \n\n Напиши мне "Помощь" или /help, чтобы узнать больше!') 
+
+
+@bot.message_handler(commands = ['help'])
+def main(message):
+ bot.send_message(message.chat.id, 'Help information')
+
+
+bot.polling(none_stop=True)
+
+
+
+
