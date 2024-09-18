69b69a0a43a0e0b2aa3b827cbb52bbd974f7960d
Mr.Blood <kekcheburek335@gmail.com>
2024-09-18 22:33:26 +0500

    Fix bugs and add new async code

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import sqlite3
 from datetime import datetime, timedelta
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.filters import Command
 from aiogram import Router
 from dotenv import load_dotenv
@@ -17,20 +16,17 @@ bot = Bot(token=TG_TOKEN)
 dp = Dispatcher()
 router = Router()
 
-# Создание таблицы записей
+# Создание таблицы записей, если её нет
 def create_appointments_table() -> None:
     """Создание таблицы для записей, если её нет."""
-    conn = sqlite3.connect("appointments.db")
-    c = conn.cursor()
-    c.execute("""
-        CREATE TABLE IF NOT EXISTS appointments (
-            chat_id INTEGER,
-            appointment_date TEXT,
-            appointment_time INTEGER
-        )
-    """)
-    conn.commit()
-    conn.close()
+    with sqlite3.connect("appointments.db") as conn:
+        conn.execute("""
+            CREATE TABLE IF NOT EXISTS appointments (
+                chat_id INTEGER,
+                appointment_date TEXT,
+                appointment_time INTEGER
+            )
+        """)
 
 # Асинхронная функция для отправки сообщения с разметкой
 async def send_message_with_markup(chat_id: int, text: str, markup: ReplyKeyboardMarkup) -> None:
@@ -68,24 +64,15 @@ def format_help_message() -> str:
 def create_help_markup() -> ReplyKeyboardMarkup:
     """Создание клавиатуры для меню помощи."""
     buttons = [
-        [
-            types.KeyboardButton(text="Хочу идеи для маникюра💅"),
-            types.KeyboardButton(text="Фотогалерея📸"),
-        ],
-        [
-            types.KeyboardButton(text="Тест💎"),
-            types.KeyboardButton(text="Советы по уходу💖"),
-        ],
-        [
-            types.KeyboardButton(text="Ногти и здоровье🩺"),
-            types.KeyboardButton(text="Модные тренды🔥"),
-        ],
-        [
-            types.KeyboardButton(text="Записаться на процедуру🗓"),
-            types.KeyboardButton(text="Цены и услуги💲"),
-        ],
+        [types.KeyboardButton(text="Хочу идеи для маникюра💅"),
+         types.KeyboardButton(text="Фотогалерея📸")],
+        [types.KeyboardButton(text="Тест💎"),
+         types.KeyboardButton(text="Советы по уходу💖")],
+        [types.KeyboardButton(text="Ногти и здоровье🩺"),
+         types.KeyboardButton(text="Модные тренды🔥")],
+        [types.KeyboardButton(text="Записаться на процедуру🗓"),
+         types.KeyboardButton(text="Цены и услуги💲")]
     ]
-
     return types.ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
 
 # Обработка команды /start
@@ -97,7 +84,8 @@ async def handle_start(message: types.Message):
     markup = create_help_markup()
     await send_message_with_markup(message.chat.id, greeting_text, markup)
 
-# Обработка команды /help некоммата лох
+# Обработка команды /help
+@router.message(Command("help"))
 async def handle_help(message: types.Message):
     """Обработка команды /help и отправка сообщения помощи."""
     help_text = format_help_message()
@@ -106,85 +94,73 @@ async def handle_help(message: types.Message):
 
 # Создание разметки для выбора времени записи
 def create_appointment_markup(dates, unavailable_time_slots):
-    """Создание клавиатуры для выбора времени записи."""
-    buttons = []
-    
-    # Генерация кнопок по датам и времени
-    for date in dates:
-        for time in range(16, 23, 2):  # Интервал 16:00-22:00 с шагом 2 часа
-            if (date, time) not in unavailable_time_slots:
-                button_text = f"{time}:00"
-                callback_data = f"appointment_{date}_{time}"
-                buttons.append(InlineKeyboardButton(text=button_text, callback_data=callback_data))
-    
-    # Проверка, есть ли кнопки
-    if buttons:
-        # Создаем разметку с кнопками
-        markup = InlineKeyboardMarkup(inline_keyboard=[buttons])
-    else:
-        # Если нет доступных интервалов времени, создаем разметку с одной кнопкой
-        markup = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(text="Нет доступных интервалов", callback_data="no_slots")]
-        ])
-    
-    return markup
+    buttons = [
+        [InlineKeyboardButton(text=f"{time}:00", callback_data=f"appointment_{date}_{time}")]
+        for date in dates
+        for time in range(16, 23, 2)
+        if (date, time) not in unavailable_time_slots
+    ]
 
+    # If no buttons are available, show a message
+    if not buttons:
+        buttons = [[InlineKeyboardButton(text="Нет доступных интервалов", callback_data="no_slots")]]
 
+    return InlineKeyboardMarkup(inline_keyboard=buttons)
 
-# Обработка текстовых сообщений для записи на процедуру
+# Обработка сообщений для записи на процедуру
 @router.message(lambda message: message.text.lower().startswith("записаться"))
 async def handle_appointment_message(message: types.Message):
     """Обработка сообщения для записи на процедуру."""
-    today = datetime.today().strftime("%Y-%m-%d")
-    tomorrow = (datetime.today() + timedelta(days=1)).strftime("%Y-%m-%d")
-    dates = [today, tomorrow]
+    today = datetime.today().strftime("%m-%d")
+    dates = [(datetime.today() + timedelta(days=i)).strftime("%m-%d") for i in range(1)]  # На день вперед
 
-    # Подключение к базе данных
-    conn = sqlite3.connect("appointments.db")
-    c = conn.cursor()
-
-    # Получение существующих записей на сегодня и завтра
-    c.execute("SELECT appointment_date, appointment_time FROM appointments WHERE appointment_date IN (?, ?)", (today, tomorrow))
-    existing_appointments = c.fetchall()
-
-    # Проверка существующих записей пользователя
-    user_id = message.chat.id
-    c.execute("SELECT * FROM appointments WHERE chat_id = ? AND appointment_date IN (?, ?)", (user_id, today, tomorrow))
-    existing_user_appointments = c.fetchall()
+    with sqlite3.connect("appointments.db") as conn:
+        c = conn.cursor()
+        # Получение существующих записей пользователя
+        user_id = message.chat.id
+        c.execute("SELECT appointment_date FROM appointments WHERE chat_id = ?", (user_id,))
+        existing_user_appointments = c.fetchall()
+        existing_user_dates = {appointment[0] for appointment in existing_user_appointments}
 
     # Определение недоступных временных интервалов
-    unavailable_time_slots = set()
-    for appointment in existing_appointments:
-        date, time = appointment
-        unavailable_time_slots.add((date, time))
+    unavailable_time_slots = {(date, time) for date in existing_user_dates for time in range(16, 23, 2)}
 
-    if existing_user_appointments:
-        for appointment in existing_user_appointments:
-            date, time = appointment[1], appointment[2]
-            unavailable_time_slots.add((date, time))
+    # Проверка, есть ли у пользователя запись на сегодня
+    c.execute("SELECT appointment_time FROM appointments WHERE chat_id = ? AND appointment_date = ?", (user_id, today))
+    existing_today_appointments = c.fetchall()
+    existing_today_times = {appointment[0] for appointment in existing_today_appointments}
+
+    # Добавление временных слотов, которые уже заняты сегодня
+    unavailable_time_slots.update((today, time) for time in existing_today_times)
 
     # Создание клавиатуры для выбора времени
-    markup = create_appointment_markup(dates, unavailable_time_slots)
-    await send_message_with_markup(message.chat.id, "Выберите время для записи на процедуру:", markup)
+    for date in dates:
+        markup = create_appointment_markup([date], unavailable_time_slots)
+        await bot.send_message(message.chat.id, f"Запись на {date}:")
+        await send_message_with_markup(message.chat.id, "Выберите время для записи на процедуру:", markup)
 
-    conn.close()
 
 # Обработка колбеков для записи на процедуру
 @router.callback_query(lambda callback: callback.data.startswith("appointment_"))
 async def handle_appointment_callback(callback: types.CallbackQuery):
     """Обработка колбеков для записи на процедуру."""
     chat_id = callback.message.chat.id
-    appointment_data = callback.data.split("_")
-    date = appointment_data[1]
-    appointment_time = int(appointment_data[2])
+    date, appointment_time = callback.data.split("_")[1:3]
+
+    # Проверка, есть ли у пользователя запись на этот день
+    with sqlite3.connect("appointments.db") as conn:
+        c = conn.cursor()
+        c.execute("SELECT * FROM appointments WHERE chat_id = ? AND appointment_date = ?", (chat_id, date))
+        existing_appointment = c.fetchone()
+
+    if existing_appointment:
+        await bot.send_message(chat_id, "Вы уже записаны на процедуру в этот день. Выберите другой день.")
+        return
 
     # Внесение записи в базу данных
-    conn = sqlite3.connect("appointments.db")
-    c = conn.cursor()
-    c.execute("INSERT INTO appointments (chat_id, appointment_date, appointment_time) VALUES (?, ?, ?)", 
-              (chat_id, date, appointment_time))
-    conn.commit()
-    conn.close()
+    with sqlite3.connect("appointments.db") as conn:
+        conn.execute("INSERT INTO appointments (chat_id, appointment_date, appointment_time) VALUES (?, ?, ?)", 
+                     (chat_id, date, int(appointment_time)))
 
     await bot.send_message(chat_id, f"Вы выбрали {appointment_time}:00 для записи на процедуру в {date}.")
 
@@ -193,24 +169,21 @@ async def handle_appointment_callback(callback: types.CallbackQuery):
 async def handle_text_message(message: types.Message):
     """Обработка текстовых сообщений и ответ на них."""
     response = get_response(message.text.lower().rstrip("💅📸💎💖🩺🔥🗓💲"))
-    if callable(response):
-        await response(message)
-    else:
-        await message.answer(response)
+    await message.answer(response)
 
 # Получение ответа на запрос пользователя
 def get_response(message_text: str) -> str:
     """Получение соответствующего ответа в зависимости от ввода пользователя."""
     responses = {
-        "хочу идеи для маникюра": "Here are some ideas for your manicure... 💅",
-        "фотогалерея": "Check out our photo gallery... 📸",
-        "тест": "Doing test... 💎",
-        "советы по уходу": "Here are some care tips for your nails... 💖",
-        "ногти и здоровье": "Learn how nail health affects your well-being... 🩺",
-        "модные тренды": "Discover the latest manicure trends... 🔥",
-        "цены и услуги": "Find out about our prices and services... 💲",
+        "хочу идеи для маникюра": "Вот несколько идей для вашего маникюра... 💅",
+        "фотогалерея": "Посмотрите нашу фотогалерею... 📸",
+        "тест": "Прохожу тест... 💎",
+        "советы по уходу": "Вот несколько советов по уходу за ногтями... 💖",
+        "ногти и здоровье": "Узнайте, как здоровье ногтей влияет на ваше самочувствие... 🩺",
+        "модные тренды": "Откройте для себя последние тренды маникюра... 🔥",
+        "цены и услуги": "Узнайте о наших ценах и услугах... 💲",
     }
-    return responses.get(message_text, "Sorry, I don't understand that. Please try again.")
+    return responses.get(message_text, "Извините, я не понимаю. Пожалуйста, попробуйте снова.")
 
 # Запуск бота
 async def main():
